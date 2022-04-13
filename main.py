22da41404db96c769e755c465b6e284581d28ac6
garrett4wade <fuwth17@gmail.com>
2022-04-13 16:40:15 +0800

    .

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from collections import deque
 import argparse
 import gym
 import logging
+import os
 import numpy as np
-import torch
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-from core import ReplayBuffer, AtariDQN, MLPDQN
+from core import ReplayBuffer
+from env_wrapper import SubprocVecEnv
 
 logging.basicConfig(
     format=
@@ -31,6 +37,34 @@ def pixel_to_float(x):
         return x
 
 
+class DQN(nn.Module):
+
+    def __init__(self, obs_dim, act_dim, linear, dueling, hidden_dim=512):
+        super().__init__()
+
+        self.fc1 = nn.Linear(obs_dim, hidden_dim)
+        self.linear = linear
+        self.dueling = dueling
+        if dueling:
+            self.V = nn.Linear(hidden_dim, 1)
+            self.A = nn.Linear(hidden_dim, act_dim)
+        else:
+            self.Q = nn.Linear(hidden_dim, act_dim)
+
+        self.device = T.device("cpu")
+
+    def forward(self, state):
+        act_fn = (lambda x: x) if self.linear else F.relu
+        flat1 = act_fn(self.fc1(state))
+        if self.dueling:
+            V = self.V(flat1)
+            A = self.A(flat1)
+
+            return V + A - A.mean(-1, keepdim=True)
+        else:
+            return self.Q(flat1)
+
+
 def make_env(env_name, eval_=False):
     env = gym.make(env_name)
     env = gym.wrappers.RecordEpisodeStatistics(env)
@@ -49,186 +83,138 @@ def make_env(env_name, eval_=False):
     return env
 
 
-def dqn_loss(q1_net,
-             q2_net,
-             s,
-             a,
-             n_s,
-             r,
-             d,
-             gamma,
-             double,
-             device=torch.device("cuda:0")):
-    s = torch.from_numpy(pixel_to_float(s)).to(device)
-    n_s = torch.from_numpy(pixel_to_float(n_s)).to(device)
-    a = torch.from_numpy(a).to(device)
-    r = torch.from_numpy(r).to(device)
-    d = torch.from_numpy(d).to(device)
-
-    if double and np.random.rand() < 0.5:
-        q1_net, q2_net = q2_net, q1_net
-
-    with torch.no_grad():
-        if double:
-            n_a = q1_net(n_s).argmax(-1, keepdim=True)
-            n_qa = q2_net(n_s).gather(-1, n_a)
-        else:
-            n_qa = q2_net(n_s).max(-1, keepdim=True).values
-        assert n_qa.shape[-1] == 1
-        # n_qa = q2_net.fc.denormalize(n_qa)
-
-        q_target = r + gamma * (1 - d) * n_qa.squeeze(-1)
-        # q_target = q_target.unsqueeze(-1)
-
-        # q1_net.fc.update(q_target)
-
-        # mu, sigma = q1_net.fc.mean_std()
-        # logger.debug(f"Update PopArt to Mean {mu.item()} Std {sigma.item()}")
-
-        # q_target = q1_net.fc.normalize(q_target)
-        # q_target = q_target.squeeze(-1)
-
-    qa = q1_net(s).gather(-1, a.unsqueeze(-1).long()).squeeze(-1)
-    assert len(qa.shape) == 1 and len(q_target.shape) == 1, (qa.shape,
-                                                             q_target.shape)
-    loss = (q_target - qa)**2
-    return loss.mean()
-
-
-def eval_dqn(q_net,
-             eval_env,
-             n_episodes=20,
-             device=torch.device("cuda:0"),
-             deterministic=False,
-             eps=0.05):
-    ep_cnt = total_ep_step = total_ep_ret = total_time = 0
+def eval_dqn(
+        agent,
+        eval_env,
+        n_episodes=20,
+        device=T.device("cuda:0")
+    if T.cuda.is_available() else T.device("cpu"),
+):
+    ep_cnt = total_ep_step = total_ep_ret = 0
+    obs = eval_env.reset()
     while ep_cnt < n_episodes:
-        obs = eval_env.reset()
-        done = False
-        while not done:
-            obs_net = torch.from_numpy(
-                pixel_to_float(obs)).unsqueeze(0).to(device)
-            act = q_net(obs_net).argmax(-1).item()
-            if not deterministic and np.random.rand() < eps:
-                act = eval_env.action_space.sample()
-            obs, _, done, info = eval_env.step(act)
-        ep_cnt += 1
-        total_ep_ret += info['episode']['r']
-        total_ep_step += info['episode']['l']
-        total_time += info['episode']['t']
-    logger.info(
-        f"Evaluation {n_episodes}, Episode Return {total_ep_ret / n_episodes}"
-        f", Episode Length {total_ep_step / n_episodes}"
-        f", Time/Episode {total_time / n_episodes}s ({total_time}s in total)")
-    return dict(eval_episode_return=total_ep_ret / n_episodes,
-                eval_episode_length=total_ep_step / n_episodes,
-                eval_time=total_time / n_episodes)
+        action = agent.choose_action(obs)
+        obs, _, done, info = eval_env.step(action)
+        for d, inf in zip(done, info):
+            if d:
+                ep_cnt += 1
+                total_ep_ret += inf['episode']['r']
+                total_ep_step += inf['episode']['l']
+    return dict(
+        eval_episode_return=total_ep_ret / n_episodes,
+        eval_episode_length=total_ep_step / n_episodes,
+    )
 
 
-def train_dqn(q1_net,
-              q2_net,
-              buffer,
-              train_env,
-              eval_env,
-              use_wandb,
-              eval_interval=500,
-              log_interval=500,
-              warmup_steps=1000,
-              total_env_steps=int(5e6),
-              lr=1e-4,
-              bs=32,
-              target_update_interval=int(1e4),
-              eps_start=1.0,
-              eps_end=0.05,
-              eps_decay_per_step=1e-3,
-              gamma=0.99,
-              double=True,
-              device=torch.device("cuda:0")):
-    q1_optimizer = torch.optim.Adam(q1_net.parameters(), lr=lr)
-    if double:
-        q2_optimizer = torch.optim.Adam(q2_net.parameters(), lr=lr)
-    else:
-        q2_optimizer = None
+class Agent():
 
-    obs = train_env.reset()
-    for _ in range(warmup_steps):
-        act = train_env.action_space.sample()
-        obs_, rew, don, info = train_env.step(act)
-        buffer.put(obs, act, obs_, rew, don)
-        if don:
-            obs_ = train_env.reset()
-        obs = obs_
+    def __init__(self,
+                 gamma,
+                 epsilon,
+                 lr,
+                 n_actions,
+                 input_dims,
+                 mem_size,
+                 batch_size,
+                 double,
+                 linear,
+                 dueling,
+                 eps_min=0.01,
+                 eps_dec=5e-7,
+                 replace=1000,
+                 chkpt_dir='tmp/dueling_ddqn'):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.replace_target_cnt = replace
+        self.chkpt_dir = chkpt_dir
+        self.action_space = [i for i in range(self.n_actions)]
+        self.learn_step_counter = 0
 
-    env_frames = warmup_steps
-    step = 0
+        self.double = double
 
-    eps = eps_start
+        self.memory = ReplayBuffer(mem_size, input_dims)
 
-    while env_frames < total_env_steps:
-        log_info = {}
+        self.q_eval = DQN(
+            *self.input_dims,
+            self.n_actions,
+            dueling=dueling,
+            linear=linear,
+        )
+        self.optimizer = T.optim.Adam(self.q_eval.parameters(), lr=lr)
 
-        obs_net = torch.from_numpy(pixel_to_float(obs)).unsqueeze(0).to(device)
-        if np.random.rand() < eps:
-            act = train_env.action_space.sample()
+        self.q_next = DQN(
+            *self.input_dims,
+            self.n_actions,
+            dueling=dueling,
+            linear=linear,
+        )
+
+    def choose_action(self, observation):
+        state = T.tensor(pixel_to_float(observation),
+                         dtype=T.float).to(self.q_eval.device)
+        q = self.q_eval.forward(state)
+        dtm_action = T.argmax(q, -1).cpu().numpy()
+        rnd_action = np.array([
+            np.random.choice(self.action_space)
+            for _ in range(observation.shape[0])
+        ])
+        mask = np.random.random(size=(observation.shape[0], )) > self.epsilon
+
+        return mask * dtm_action + (1 - mask) * rnd_action
+
+    def learn(self):
+        if self.memory.cur_size < self.batch_size:
+            return
+
+        self.optimizer.zero_grad()
+
+        if self.learn_step_counter % self.replace_target_cnt == 0:
+            self.q_next.load_state_dict(self.q_eval.state_dict())
+
+        state, action, new_state, reward, done = \
+                                self.memory.get(self.batch_size)
+
+        states = T.tensor(pixel_to_float(state)).to(self.q_eval.device)
+        rewards = T.tensor(reward).to(self.q_eval.device)
+        dones = T.tensor(done).to(self.q_eval.device)
+        actions = T.tensor(action).to(self.q_eval.device)
+        states_ = T.tensor(pixel_to_float(new_state)).to(self.q_eval.device)
+
+        indices = np.arange(self.batch_size)
+
+        q_pred = self.q_eval.forward(states)[indices, actions]
+        q_next = self.q_next.forward(states_)
+
+        q_eval = self.q_eval.forward(states_)
+
+        max_actions = T.argmax(q_eval, dim=1)
+
+        q_next[dones] = 0.0
+        if self.double:
+            q_next = q_next[indices, max_actions]
         else:
-            act = q1_net(obs_net).argmax(-1).item()
-        obs_, rew, don, info = train_env.step(act)
+            q_next = q_next.max(-1).values
+        q_target = rewards + self.gamma * q_next
 
-        buffer.put(obs, act, obs_, rew, don)
-
-        if don:
-            obs_ = train_env.reset()
-            logger.info(
-                f"Train env episode done with episode return"
-                f" {info['episode']['r']} and episode length {info['episode']['l']}"
-            )
-            log_info = dict(episode_return=info['episode']['r'],
-                            episode_length=info['episode']['l'])
-
-        obs = obs_
-
-        s, a, n_s, r, d = buffer.get(bs)
-        q1_optimizer.zero_grad()
-        if q2_optimizer is not None:
-            assert double
-            q2_optimizer.zero_grad()
-
-        loss = dqn_loss(q1_net, q2_net, s, a, n_s, r, d, gamma, double)
+        loss = ((q_target - q_pred)**2).mean()
         loss.backward()
+        self.optimizer.step()
+        self.learn_step_counter += 1
 
-        q1_optimizer.step()
-        if q2_optimizer is not None:
-            q2_optimizer.step()
-
-        log_info = dict(loss=loss.item(), **log_info)
-
-        eps = max(eps_end, eps - eps_decay_per_step)
-
-        step += 1
-        env_frames += 1
-
-        if not double and step % target_update_interval == 0:
-            q2_net.load_state_dict(q1_net.state_dict())
-            logger.info("Update target network.")
-
-        if step % eval_interval == 0:
-            eval_info = eval_dqn(q1_net, eval_env)
-            log_info = dict(eval_info, **log_info)
-
-        if step % log_interval == 0:
-            if use_wandb:
-                wandb.log(log_info, step=env_frames)
-            torch.save(q1_net.state_dict(), f"./results/ckpt_{step}.pt")
-            logger.info(
-                f"Environment steps {int(env_frames)}/{total_env_steps}, log info {log_info}"
-            )
+        self.epsilon = max(self.eps_min, self.epsilon - self.eps_dec)
+        return loss.item()
 
 
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env_name",
-                        type=str,
-                        default="SpaceInvadersNoFrameskip-v4")
+    parser.add_argument("--env_name", type=str, default="CartPole-v1")
+    parser.add_argument("--group", type=str)
     parser.add_argument("--dueling", action='store_true')
     parser.add_argument("--double", action="store_true")
     parser.add_argument("--linear", action='store_true')
@@ -236,12 +222,12 @@ def main():
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
 
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    T.backends.cudnn.deterministic = True
+    T.backends.cudnn.benchmark = True
 
     np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    T.manual_seed(args.seed)
+    T.cuda.manual_seed_all(args.seed)
 
     try:
         import wandb
@@ -257,55 +243,84 @@ def main():
         logger.info(
             "wandb not installed. Code runs fine without logging to the web.")
 
-    device = torch.device("cuda:0")
+    device = T.device("cuda:0") if T.cuda.is_available() else T.device('cpu')
 
-    train_env = make_env(args.env_name)
-    eval_env = make_env(args.env_name, eval_=True)
+    train_env = SubprocVecEnv(
+        [lambda: make_env(args.env_name) for _ in range(8)])
+    eval_env = SubprocVecEnv(
+        [lambda: make_env(args.env_name, eval_=True) for _ in range(2)])
 
     act_dim = train_env.action_space.n
-    buffer = ReplayBuffer(int(1e6), train_env.observation_space.shape, act_dim)
 
-    if "NoFrameskip" in args.env_name:
-        q1_net = AtariDQN(
-            [3, 3, 3, 3],
-            act_dim,
-            act_fn=(lambda: torch.nn.ReLU(inplace=True)
-                    if not args.linear else torch.nn.Identity()),
-            dueling=args.dueling,
-        ).to(device)
-        q2_net = AtariDQN(
-            [3, 3, 3, 3],
-            act_dim,
-            act_fn=(lambda: torch.nn.ReLU(inplace=True)
-                    if not args.linear else torch.nn.Identity()),
-            dueling=args.dueling,
-        ).to(device)
-    else:
-        q1_net = MLPDQN(
-            train_env.observation_space.shape[0],
-            act_dim,
-            act_fn=(lambda: torch.nn.ReLU(inplace=True)
-                    if not args.linear else torch.nn.Identity()),
-        ).to(device)
-        q2_net = MLPDQN(
-            train_env.observation_space.shape[0],
-            act_dim,
-            act_fn=(lambda: torch.nn.ReLU(inplace=True)
-                    if not args.linear else torch.nn.Identity()),
-        ).to(device)
-    if not args.double:
-        q2_net.load_state_dict(q1_net.state_dict())
+    total_env_steps = int(5e6)
+    eval_interval = int(1e3)
+    log_interval = int(1e3)
+    save_interval = int(5e4)
 
-    train_dqn(q1_net,
-              q2_net,
-              buffer,
-              train_env,
-              eval_env,
-              use_wandb=args.wandb)
+    agent = Agent(gamma=0.99,
+                  epsilon=1.0,
+                  lr=5e-4,
+                  input_dims=train_env.observation_space.shape,
+                  n_actions=train_env.action_space.n,
+                  mem_size=int(1e5),
+                  dueling=args.dueling,
+                  linear=args.linear,
+                  double=args.double,
+                  eps_min=0.01,
+                  batch_size=64,
+                  eps_dec=1e-3,
+                  replace=100)
+
+    scores = deque(maxlen=100)
+    losses = deque(maxlen=1000)
+
+    n_env_steps = ep_cnt = 0
+    observation = train_env.reset()
+    while n_env_steps < total_env_steps:
+        action = agent.choose_action(observation)
+        observation_, reward, done, info = train_env.step(action)
+
+        agent.memory.put_batch(observation, action, observation_, reward, done)
+        loss = agent.learn()
+
+        if loss is not None:
+            losses.append(loss)
+
+        observation = observation_
+
+        for d, inf in zip(done, info):
+            if d:
+                ep_cnt += 1
+                scores.append(inf['episode']['r'])
+
+        n_env_steps += 1
+
+        if n_env_steps % log_interval == 0:
+            avg_score = np.mean(scores)
+            logger.info(''.join([
+                f'Environment Steps {n_env_steps}/{total_env_steps}',
+                f'\t Number of Episodes: {ep_cnt}',
+                '\t Average Score %.1f' % avg_score,
+                '\t Epsilon %.2f' % agent.epsilon
+            ]))
+            if wandb_run is not None and args.wandb:
+                wandb.log(dict(train_episode_return=avg_score, eps=agent.epsilon, loss=np.mean(losses)), n_env_steps)
+
+        if n_env_steps % eval_interval == 0:
+            eval_info = eval_dqn(agent, eval_env)
+            logger.info(
+                f"Evaluation Episode Return {eval_info['eval_episode_return']}"
+                f", Episode Length {eval_info['eval_episode_length']}.")
+            if wandb_run is not None and args.wandb:
+                wandb.log(eval_info, n_env_steps)
+
+        if n_env_steps % save_interval == 0:
+            fname = "dqn"
+            if args.linear:
+                fname += "_linear"
+            if args.double:
+                fname += "_double"
+            T.save(agent.q_eval.state_dict(), f"results/{fname}_{n_env_steps}.pt")
 
     if wandb_run is not None:
         wandb_run.finish()
-
-
-if __name__ == "__main__":
-    main()
